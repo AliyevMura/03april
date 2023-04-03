48bf062165b4408426b5d76e2179d86a2ae25191
AliyevMura <7lvghl9@code.edu.az>
2023-04-03 21:31:29 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+int num = 14;
+
+if (num>10 && num < 20)
+{
+
+    if(num % 2 == 0)
+    {
+        Console.WriteLine("Cut ededdir");
+    }
+
+    else
+    {
+        Console.WriteLine("tek ededdir");
+    }
+}
+else
+{
+    Console.WriteLine("eded bu araliqda deyil");
+}
+
+
+
+
+
+   
